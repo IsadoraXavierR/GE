69154ab76046acf85471ce2b6cb1bf4d5f9867d8
IsadoraXavierR <100290446+IsadoraXavierR@users.noreply.github.com>
2022-03-25 22:57:51 -0300

    correção

--- GE.js/index.js
+++ GE.js/index.js
@@ -1,28 +1,28 @@
-function bruxa(params) {
+function bruxa() {
     var bruxa=prompt("se digitar (sim) Você se tornará uma bruxa rara e muito poderosa que é a mistura de uma bruxa com um caçador,quando seu pai descobre a verdade sobre vocês resolve fugir com vocês e acaba morrendo por vocês duas está preparado(a)?")
        if (bruxa=="sim")
         {
-            location.href ="../html.html/jaca.html"
+            location.href ="./html.html/jaca.html"
         console.log( "Bem-vinde")
        } else {
            console.log("desculpa")
        }
 }
-function guerreiro(params) {
+function guerreiro() {
     var guerreiro=prompt("tem certeza?")
        if (guerreiro=="sim")
         {
-            location.href ="minha.html"
+            location.href ="./minha.html"
         console.log( "Bem-vinde")
        } else {
            console.log("desculpa")
        }
 }
-function caçador(params) {
+function caçador() {
     var caçador=prompt("tem certeza?")
        if (caçador=="sim")
         {
-            location.href ="nossa.html"
+            location.href ="./nossa.html"
         console.log( "Bem-vinde")
        } else {
            console.log("desculpa")
