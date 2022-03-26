2a4040580f211c25734a0da724dd685b3aee781e
IsadoraXavierR <100290446+IsadoraXavierR@users.noreply.github.com>
2022-03-25 23:15:57 -0300

    Update jacascript.js

--- GE.js/jacascript.js
+++ GE.js/jacascript.js
@@ -2,7 +2,7 @@ function oFim() {
     var hist=prompt("o que você deseja fazer?")
        if (hist=="A")
         {
-            location.href ="./fim.html"
+            location.href ="./html.html/fim.html"
        } else {
            alert("tente de novo ou abra o console")
            console.log("você consegue matar o dragão,mas acham que você é um bruxo e te levam pra inquisição onde te matam");
