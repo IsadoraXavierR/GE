c34ce501dc94de75d45280192dc09861433d9424
IsadoraXavierR <100290446+IsadoraXavierR@users.noreply.github.com>
2022-03-25 23:25:21 -0300

    Update jaca.js

--- GE.js/jaca.js
+++ GE.js/jaca.js
@@ -2,7 +2,7 @@ function angel() {
     var angel=prompt("o que você deseja fazer?")
        if (angel=="Ajudar ele"|| angel=="B")
         {
-            location.href ="./html.html/dela.html"
+            location.href ="./dela.html"
        } else {
            alert("tente de novo")
            console.log("quando você dá as costas,descobre que era uma armadilha de caçadores que te matam");
