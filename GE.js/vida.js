37170e2edf4428394278f19e984298dcc2c5160f
IsadoraXavierR <100290446+IsadoraXavierR@users.noreply.github.com>
2022-03-25 23:20:10 -0300

    Update vida.js

--- GE.js/vida.js
+++ GE.js/vida.js
@@ -1,7 +1,7 @@
 function sFim(params) {
     var briga =prompt("o que vai fazer?")
     if (briga=="B") {
-        location.href="./fim.html"
+        location.href="./html.html/fim.html"
     } else {
         alert ("tente de novo")
         console.log("seu amigo te mata no meio da viagem, mata seu lobo e fica com suas terras");
