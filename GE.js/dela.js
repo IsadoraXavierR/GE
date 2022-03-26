8d6a283c85b15c8c21d339122792a38a23799933
IsadoraXavierR <100290446+IsadoraXavierR@users.noreply.github.com>
2022-03-25 23:15:16 -0300

    Update dela.js

--- GE.js/dela.js
+++ GE.js/dela.js
@@ -2,7 +2,7 @@ function mae() {
     var mae=prompt("o que você deseja fazer?")
        if (mae=="treinar com o Bartolomeu"|| mae=="A"||mae=="ir terinar")
         {
-            location.href ="./antonieta.html"
+            location.href ="./html.html/antonieta.html"
        } else {
           alert("tente de novo")
         console.log("quando estão voltando descobrem que sua casa foi encendiada, e perde a ultima coisa que ligava ao seu pai")
