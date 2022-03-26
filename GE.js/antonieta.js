cea959e8bc27972baa1b5caf7ce6cb7b85e7568d
IsadoraXavierR <100290446+IsadoraXavierR@users.noreply.github.com>
2022-03-25 23:14:24 -0300

    Update antonieta.js

--- GE.js/antonieta.js
+++ GE.js/antonieta.js
@@ -2,7 +2,7 @@ function pFim() {
     var sonho=prompt("o que você deseja fazer?")
        if (sonho=="Liberar os poderes"|| sonho=="C")
         {
-            location.href ="./fim.html"
+            location.href ="./html.html/fim.html"
        } else {
           alert("tente de novo")
         console.log("ela mata o bartolomeu e manda seus homens matarem seus colegas")
